ff20acd24177afb3442396d3bedaa28e2b64343a
Khalid Supreme <richunclekhalid@gmail.com>
2023-11-14 13:34:58 +0100

    fix: modify 'vuetify' package configuration

--- src/main.js
+++ src/main.js
@@ -2,12 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import { createPinia } from 'pinia'
-
-import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-
 loadFonts()
- 
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
 const app = createApp(App);
 app.use(createPinia());
 app.use(vuetify);
