7b4ed0f7ec8dec93ec3db7479a7a85044d5e065b
Khalid Supreme <richunclekhalid@gmail.com>
2023-11-15 12:13:10 +0100

    update: modify styling

--- src/components/JottingsOverview.vue
+++ src/components/JottingsOverview.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
-        <v-card class="my-4 px2" :title="jotting.title">
+        <v-card class="my-4 px2">
+            <v-card-item>
+                <v-card-title class="jotting-title">
+                    {{ jotting.title }}
+                </v-card-title>
+            </v-card-item>
             <v-card-actions>
 
                 <v-spacer></v-spacer>
                 
-            
-                <v-btn size="small" color="success" icon="mdi-delete">
+                <v-btn size="large" color="success" icon="mdi-delete">
                 </v-btn>
-                <v-btn size="small" color="success" icon="mdi-heart-outline">
-
-                </v-btn>
+                <v-btn size="large" color="success" icon="mdi-heart-outline">
+            </v-btn>
             </v-card-actions>
             <v-divider></v-divider>
         </v-card>
