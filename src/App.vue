5032889fdc38c9f3393b7c63dcedc8bec2db9dc9
Khalid Supreme <richunclekhalid@gmail.com>
2023-11-15 13:04:18 +0100

    feat: display jotting based on condition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 
 <template>
     <v-sheet elevation="12" max-width="1000" rounded="lg" width="100%" class="pa-4 mx-auto">
-        <!-- our html code will go here -->
+        
         <!-- heading -->
         <header class="jotta-header">
             <img src="./assets/pinia-logo.svg" alt="pinia-logo">
@@ -10,8 +10,17 @@
 
         <!-- jottings -->
         <div class="jottings">
+
+            <!-- all jottings -->
+            <p>all</p>
             <div v-for="jotting in jottaStore.jottings" :key="jotting.id">
-                <!-- <p>{{ jotting.title }}</p> -->
+                <Jottings :jotting="jotting" />
+
+            </div>
+            
+            <!-- favorite jottings -->
+            <p>favorites</p>
+            <div v-for="jotting in jottaStore.fave" :key="jotting.id">
                 <Jottings :jotting="jotting" />
 
             </div>
--- src/stores/JottaStore.js
+++ src/stores/JottaStore.js
@@ -1,13 +1,19 @@
 import { defineStore } from "pinia";
-
 export const useJottaStore = defineStore('jottastore', {
     state: () => ({
         // add some obj properties here
         jottings: [
-            { id: 1, title: 'Evolution of AI', isFave: false },
+            { id: 1, title: 'Evolution of AI', isFave: true },
             { id: 2, title: 'How to make jollof rice', isFave: false }
         ],
 
         name: 'Jotta'
-    })
+    }),
+
+    getters: {
+        fave(
+        ) {
+            return this.jottings.filter(J => J.isFave)
+        }
+    },
 });
